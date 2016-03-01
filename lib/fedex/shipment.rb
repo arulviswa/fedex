79033b84d5e129766b79e2ce398077ebf7388a33
Viswanathan P <viswanathan@neevtech.com>
2016-03-01 18:03:19 +0530

    Fixed issues

--- lib/fedex/shipment.rb
+++ lib/fedex/shipment.rb
@@ -9,6 +9,7 @@ require 'fedex/request/ground_close'
 require 'fedex/request/pickup'
 require 'fedex/request/pickup_availability'
 require 'fedex/request/service_availability'
+require 'fedex/request/cancel_pickup'
 
 module Fedex
   class Shipment
